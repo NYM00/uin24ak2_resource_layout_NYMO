b42cb5ef2d5aacb1ed24f35a7b13d9e87aa4f55f
NYM00 <victor.nymo@gmail.com>
2024-01-23 14:41:06 +0100

    Klart å lage funksjon som skriver ut resources
    Funksjonen skriver ut hele resources i containter.

--- webscript.js
+++ webscript.js
@@ -2,12 +2,13 @@ console.log(resources)
 
 let resourcesHTML = "<h2>HTML</H2>"
 
-resources.map(resource => resourcesHTML += `<section>
+resources.map(resource => resourcesHTML += `<artice>
 <h3>${resource.category}</h3>
 <p>${resource.text}</p>
 <ul><li>${resource.sources}</li></ul>
-</section>
+</article>
 `)
 
 const main = document.getElementsByTagName("main")
 main[0].innerHTML = resourcesHTML
+
